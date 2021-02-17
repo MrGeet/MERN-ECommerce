f38b4e318340a239d6740b3f399b4a935aeef470
MrGeet <aldarbatomunkuev@gmail.com>
2021-02-17 13:22:13 +1300

    added rating component, home screen and so on

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,21 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import { Container } from 'react-bootstrap';
+import './bootstrap.min.css';
+import './index.css';
+
 const App = () => {
 	return (
 		<>
-			<h1>Welcome to EasyShops</h1>
+			<Header />
+			<main className="py-3">
+				<Container>
+					<HomeScreen />
+				</Container>
+			</main>
+			<Footer />
 		</>
 	);
 };
