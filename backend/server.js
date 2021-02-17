dea73a9658137fb8a680ecfb07aa87642a181b04
MrGeet <aldarbatomunkuev@gmail.com>
2021-02-18 12:13:12 +1300

    adding .env file boom

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const products = require('./data/products');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('RUNNING API LOL');
+});
+
+app.get('/api/products', (req, res) => {
+	res.json(products);
+});
+
+app.get('/api/product/:id', (req, res) => {
+	const product = products.find((p) => p._id === req.params.id);
+	res.json(product);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
