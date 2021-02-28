2a1b38ab59c4963dbb3a8374e155708c893b821d
MrGeet <aldarbatomunkuev@gmail.com>
2021-02-28 20:54:19 +1300

    adding autherization, adding password middleware to user model

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -27,6 +27,39 @@ const authUser = asyncHandler(async (req, res) => {
 	}
 });
 
+//@desc registering a new user
+//@route Post /api/users/login
+//@access Public
+const registerUser = asyncHandler(async (req, res) => {
+	const { name, email, password } = req.body;
+
+	const userExists = await User.findOne({ email });
+
+	if (userExists) {
+		res.status(400);
+		throw new Error('Current email is already used');
+	}
+
+	const user = await User.create({
+		name,
+		email,
+		password,
+	});
+
+	if (user) {
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+			token: generateToken(user._id),
+		});
+	} else {
+		res.status(400);
+		throw new Error('Invalid data supplied');
+	}
+});
+
 //@desc get user profile
 //@route Post /api/users/profile
 //@access Private
@@ -46,4 +79,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
 	}
 });
 
-export { authUser, getUserProfile };
+export { authUser, registerUser, getUserProfile };
