22f8ec284d0b0524241956e7ad78c4bb511a3a9d
MrGeet <aldarbatomunkuev@gmail.com>
2021-03-02 15:17:21 +1300

    adding register screen, update profile route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -79,4 +79,41 @@ const getUserProfile = asyncHandler(async (req, res) => {
 	}
 });
 
-export { authUser, registerUser, getUserProfile };
+//@desc get user profile
+//@route Put /api/users/profile
+//@access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+	const user = await User.findById(req.user._id);
+
+	if (user) {
+		user.name = req.body.name || user.name;
+		user.email = req.body.email || user.email;
+		if (req.body.password) {
+			user.password = req.body.password;
+		}
+
+		const updatedUser = await user.save();
+
+		res.json({
+			_id: updatedUser._id,
+			name: updatedUser.name,
+			email: updatedUser.email,
+			isAdmin: updatedUser.isAdmin,
+			token: generateToken(updatedUser._id),
+		});
+	} else {
+		res.status(404);
+		throw new Error('User not found');
+	}
+
+	if (user) {
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+		});
+	}
+});
+
+export { authUser, registerUser, getUserProfile, updateUserProfile };
