3292655f02b8844cf91f64190282f12a231b3823
MrGeet <aldarbatomunkuev@gmail.com>
2021-03-04 15:33:55 +1300

    adding order screen

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -9,6 +9,7 @@ import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
+import OrderScreen from './screens/OrderScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 
 import { Container } from 'react-bootstrap';
@@ -27,6 +28,7 @@ const App = () => {
 					<Route path="/profile" component={ProfileScreen} />
 					<Route path="/shipping" component={ShippingScreen} />
 					<Route path="/payment" component={PaymentScreen} />
+					<Route path="/order/:id" component={OrderScreen} />
 					<Route path="/placeorder" component={PlaceOrderScreen} />
 					<Route path="/product/:id" component={ProductScreen} />
 					<Route path="/cart/:id?" component={CartScreen} />
