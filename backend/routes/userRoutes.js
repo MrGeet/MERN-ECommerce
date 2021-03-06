e5f7b64fd4f08a52e92ca86b9f946d2d0f19554e
MrGeet <aldarbatomunkuev@gmail.com>
2021-03-06 17:48:05 +1300

    adding get and update user routes and controllers for admins to access

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,14 +6,21 @@ import {
 	updateUserProfile,
 	getUsers,
 	deleteUser,
+	getUserById,
+	updateUser,
 } from '../controllers/userController.js';
 import { protect, isAdmin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/login', authUser);
+
 router.route('/').post(registerUser).get(protect, isAdmin, getUsers);
+
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+
 router.delete('/:id', protect, isAdmin, deleteUser);
+router.get('/:id', protect, isAdmin, getUserById);
+router.put('/:id', protect, isAdmin, updateUser);
 
 export default router;
