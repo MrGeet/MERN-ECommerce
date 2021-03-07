6a404a777d1444120bc9f3bccfad88c005a6759f
MrGeet <aldarbatomunkuev@gmail.com>
2021-03-07 16:41:08 +1300

    created an edit screen page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -14,6 +14,7 @@ import UserEditScreen from './screens/UserEditScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import UserListScreen from './screens/UserListScreen';
 import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
 
 import { Container } from 'react-bootstrap';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
@@ -38,6 +39,7 @@ const App = () => {
 					<Route path="/admin/userlist" component={UserListScreen} />
 					<Route path="/admin/productlist" component={ProductListScreen} />
 					<Route path="/admin/user/:id/edit" component={UserEditScreen} />
+					<Route path="/admin/product/:id/edit" component={ProductEditScreen} />
 					<Route path="/" component={HomeScreen} exact />
 				</Container>
 			</main>
