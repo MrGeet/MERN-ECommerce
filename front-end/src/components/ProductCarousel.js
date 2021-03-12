d40fb789d89b6e0abc5c6b6b7ca1ca7e68aa80e2
MrGeet <aldarbatomunkuev@gmail.com>
2021-03-12 20:31:45 +1300

    fixing carousel

--- front-end/src/components/ProductCarousel.js
+++ front-end/src/components/ProductCarousel.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Carousel, Image } from 'react-bootstrap';
+import { Carousel, Image, Container, Row, Col } from 'react-bootstrap';
 import Loader from './Loader';
 import Message from './Message';
 import { listTopProducts } from '../actions/productActions';
@@ -22,16 +22,48 @@ const ProductCarousel = () => {
 		<Message variant="danger">{error}</Message>
 	) : (
 		<Carousel pause="hover" className="bg-dark">
+			<Carousel.Item>
+				<Container>
+					<Container>
+						<Row className="justify-content-md-center">
+							<Image src="images/Galaxy_s21.png" alt="Galaxy S21" fluid />
+						</Row>
+						<Row className="justify-content-md-center">
+							<Carousel.Caption>
+								<h2>New Samsung Galaxy S21</h2>
+							</Carousel.Caption>
+						</Row>
+					</Container>
+				</Container>
+			</Carousel.Item>
+
+			<Carousel.Item>
+				<Container>
+					<Container>
+						<Row className="justify-content-md-center">
+							<Image src="images/sale.png" alt="Galaxy S21" fluid />
+						</Row>
+						<Row className="justify-content-md-center">
+							<Carousel.Caption>
+								<h2>Crazy Discounts</h2>
+							</Carousel.Caption>
+						</Row>
+					</Container>
+				</Container>
+			</Carousel.Item>
 			{products.map((product) => (
 				<Carousel.Item key={product._id}>
 					<Link to={`/product/${product._id}`}>
-						<Image src={product.image} alt={product.name} fluid />
-
-						<Carousel.Caption className="carousel-caption">
-							<h2>
-								{product.name} (${product.price})
-							</h2>
-						</Carousel.Caption>
+						<Container>
+							<Row className="justify-content-md-center align-items-end">
+								<Image src={product.image} alt={product.name} fluid />
+							</Row>
+							<Row className="justify-content-md-center">
+								<Carousel.Caption className="carousel-caption">
+									<h2>{product.name}</h2>
+								</Carousel.Caption>
+							</Row>
+						</Container>
 					</Link>
 				</Carousel.Item>
 			))}
